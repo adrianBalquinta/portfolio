8f29908118bcf604e1a5c2964b79910351e9df09
Adrian <adrian.balquinta@gmail.com>
2023-07-27 07:19:35 -0500

    Rearranged html, moved javascript links to functions.php

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
 <?php wp_footer( );  ?>
+<footer class="footer py-3">
+        <div class="container">
+            <p class="small mb-0 text-light">
+                &copy; <script>document.write(new Date().getFullYear())</script> Created By <span class="text-danger">Adrian Balquinta</span></a> 
+            </p>
+        </div>
+    </footer>
 </body>
 </html>
