cf318db078761e1c0994aeedeb251e8c4960ac36
Adrian <adrian.balquinta@gmail.com>
2023-07-27 03:12:12 -0500

    created template files to better manage WordPress hierarchy

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-This is my pordfolio.
+<?php
+get_header();
+?>
+
+<h1>This is my pordfolio.</h1>
 <h4 class="header-subtitle">Hello, I am</h4>
 <h1 class="header-title"><?php bloginfo('name') ?></h1>
+
+<?php
+get_footer();
+?>
