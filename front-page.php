6daf3ce3afa357b553a2f862f84001f4b424fc50
Adrian <adrian.balquinta@gmail.com>
2023-07-27 03:28:54 -0500

    changed home page to front-page.php

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+
+<h4 class="header-subtitle">Hello, I am</h4>
+<h1 class="header-title"><?php bloginfo('name') ?></h1>
+
+<?php
+get_footer();
+?>
--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<?php
-get_header();
-?>
-
-<h1>This is my pordfolio.</h1>
-<h4 class="header-subtitle">Hello, I am</h4>
-<h1 class="header-title"><?php bloginfo('name') ?></h1>
-
-<?php
-get_footer();
-?>
+This is the generic blog listing screen template.
