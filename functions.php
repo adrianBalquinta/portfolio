55a8a904795e39d873c9ae063630b6db03e173a5
Adrian <adrian.balquinta@gmail.com>
2023-07-27 07:23:03 -0500

    Added links to javascript, css, boostrap, custom version of JQuery and isotope

--- functions.php
+++ functions.php
@@ -1,4 +1,21 @@
 <?php
+
+function portfolio_files() {
+  wp_deregister_script('jquery'); // Deregister the default jQuery that comes with WordPress
+    wp_enqueue_script('jquery', get_theme_file_uri('/assets/vendors/jquery/jquery-3.4.1.js'), array(), '3.4.1', true);
+  wp_enqueue_script('bootstrap', get_theme_file_uri('/assets/vendors/bootstrap/bootstrap.bundle.js'), array('jquery'), '4.3.1', true);
+  wp_enqueue_script('bootstrap-affix', get_theme_file_uri('/assets/vendors/bootstrap/bootstrap.affix.js'), array('jquery'), '3.3.6', true);
+  wp_enqueue_script('isotope', get_theme_file_uri('/assets/vendors/isotope/isotope.pkgd.js'), array('jquery'), '3.0.6', true);
+  
+  //JohnDoe js
+  wp_enqueue_script('john-doe-js', get_theme_file_uri('/assets/js/johndoe.js'), array(), '1.0', true);
+  //Bootstrap + JohnDoe main styles
+  wp_enqueue_style('bootstrap-css', get_theme_file_uri('/assets/css/johndoe.css'));
+  wp_enqueue_style('font_awesome', get_theme_file_uri('/assets/vendors/themify-icons/css/themify-icons.css'));
+  wp_enqueue_style('my_main_styles', get_theme_file_uri('/style.css'));
+  
+}
+  
 function my_custom_theme_setup() {
   // Add support for featured images
   add_theme_support('post-thumbnails');
@@ -15,7 +32,7 @@ function my_custom_features(){
     add_theme_support( 'title-tag');
 }
     
-
-add_action('after_setup_theme', 'my_custom_theme_setup');
-add_action('after_setup_theme', 'my_custom_features' );
+add_action('wp_enqueue_scripts','portfolio_files');
+add_action('after_setup_theme','my_custom_theme_setup');
+add_action('after_setup_theme','my_custom_features');
 ?>
