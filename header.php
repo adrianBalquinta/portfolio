cca693d68c729c02a9eb3ef696e458ea57a07aa8
Adrian <adrian.balquinta@gmail.com>
2024-02-14 01:20:21 -0600

    Added Id 'header' to use with GelElementBy Id

--- header.php
+++ header.php
@@ -9,18 +9,38 @@
     <?php wp_head(); ?>
 </head>
 <body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
-    <header class="header">
+    <header class="header" id='header'>
         <div class="container">
-            <ul class="social-icons pt-3">
-                <li class="social-item"><a class="social-link text-light" href="https://www.youtube.com/@adrianbalquinta9994" target="_blank"><i class="ti-youtube" aria-hidden="true"></i></a></li>
-                <li class="social-item"><a class="social-link text-light" href="#" target="_blank"><i class="ti-linkedin" aria-hidden="true"></i></a></li>
-                <li class="social-item"><a class="social-link text-light" href="https://github.com/adrianBalquinta?tab=repositories" target="_blank"><i class="ti-github" aria-hidden="true"></i></a></li>
-            </ul>  
-            <div class="header-content">
-                <h4 class="header-subtitle">Hello, I am</h4>
-                <h1 class="header-title"><?php bloginfo('name') ?></h1>
-                <h6 class="header-mono" >Jr Web Designer | Developer</h6>
-                <button class="btn btn-primary btn-rounded"><i class="ti-printer pr-2"></i>Print Resume</button>
-            </div>
+        <?php
+            dynamic_sidebar('links-1');
+            get_template_part('template-parts/banner','title');
+        ?>
+
         </div>
-    </header>  
+    </header>
+    <?php
+    require_once get_template_directory() . '/classes/Class_bootstrap_navwalker.php';
+    ?>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white" data-spy="affix" data-offset-top="510" id="navbar">
+        <div class="container">
+            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <?php
+
+                wp_nav_menu(array(
+                    'theme_location' => 'headerMenu',
+                    'container' => '',
+                    'container_class' => '',
+                    'container_id' => '',
+                    //'items_wrap' => '<div class="container">',
+                    'menu_class' => 'navbar-nav mr-auto',
+                    'menu_id' => '/n',
+                    'fallback_cb' => false,
+                    //'depth' => 2,
+                    'walker' => new Custom_NavWalker() // Custom nav walker for Bootstrap compatibility
+                ));?>
+
+
+        </div>
+    </nav> 
