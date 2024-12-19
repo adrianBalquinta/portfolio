f0d38f71d4ade200493de24ef58837c0a5c59a2c
Adrian <adrian.balquinta@gmail.com>
2024-12-19 03:00:48 -0500

    Changed Id sections in index.php/ Implemented new layout for Resume section/ Created .gitignore file.

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 get_header();
 ?>
     <div class="container-fluid">
-        <div id="about" class="row about-section">
+        <div id="About" class="row about-section">
     
         <?php
             $about_me = new WP_query(array(
@@ -35,7 +35,7 @@ get_header();
                 }
             } else {
 
-                get_template_parts('template-parts/content', 'none');
+                get_template_part('template-parts/content', 'none');
 
             }
 
@@ -45,11 +45,12 @@ get_header();
     </div>
 
  <!--Resume Section-->
+ <!--
     <section class="section" id="resume">
         <div class="container">
             <h2 class="mb-5"><span class="text-danger">My</span> Resume</h2>
            <div class="row">
-<?php
+/*
                 $resume = new WP_query(array(
                     'posts_per_page' => 4,
                     'post_type' => 'resume'
@@ -66,14 +67,104 @@ get_header();
                     }
                 } else {
 
-                    get_template_parts('template-parts/content', 'none');
+                    get_template_part('template-parts/content', 'none');
 
                 }
 
-            ?>
-            </div><!-- end row -->
+            */
+            </div> end row
         </div>
-    </section><!-- end resume section -->
+    </section> end resume section -->
+
+
+    <!-- refactor-->
+    <section class="section" id="Resume">
+    <div class="container">
+        <h2 class="mb-5"><span class="text-danger">My</span> Resume</h2>
+        <div class="row">
+
+            <?php
+            // Fetch Resume Posts
+            $resume = new WP_query(array(
+                'posts_per_page' => -1, // Fetch all posts
+                'post_type'      => 'resume'
+            ));
+
+            // Initialize empty arrays to group posts
+            $work_experience = [];
+            $education = null;
+            $skills = null;
+            $languages = null;
+
+            // Group posts by title
+            if ($resume->have_posts()) {
+                while ($resume->have_posts()) {
+                    $resume->the_post();
+                    //echo get_the_title();
+                    switch (get_the_title()) {
+                        case 'Work Experience':
+                            $work_experience = get_the_ID();
+                            break;
+                        case 'Education':
+                            $education = get_the_ID();
+                            break;
+                        case 'Skills':
+                            $skills = get_the_ID();
+                            break;
+                        case 'Languages':
+                            $languages = get_the_ID();
+                            break;
+                    }
+                }
+                wp_reset_postdata();
+            }
+            ?>
+
+            <!-- Left Column: First Work Experience -->
+            <div class="col-lg-4 d-flex flex-column">
+                <?php if (!empty($work_experience)) {
+                    $post = get_post($work_experience);
+                    setup_postdata($post);
+                    get_template_part('template-parts/content', 'excerpt', ['section' => 'work1']);
+                    wp_reset_postdata();
+                } ?>
+            </div>
+
+            <!-- Center Column: Second Work Experience + Education -->
+            <div class="col-lg-4 d-flex flex-column">
+                <?php if (!empty($work_experience)) {
+                    $post = get_post($work_experience);
+                    setup_postdata($post);
+                    get_template_part('template-parts/content', 'excerpt', ['section' => 'work2']);
+                    wp_reset_postdata();
+                } ?>
+                <?php if ($education) {
+                    $post = get_post($education);
+                    setup_postdata($post);
+                    get_template_part('template-parts/content', 'excerpt', ['section' => 'education']);
+                    wp_reset_postdata();
+                } ?>
+            </div>
+
+            <!-- Right Column: Skills + Languages -->
+            <div class="col-lg-4 d-flex flex-column">
+                <?php if ($skills) {
+                    $post = get_post($skills);
+                    setup_postdata($post);
+                    get_template_part('template-parts/content', 'excerpt', ['section' => 'skills']);
+                    wp_reset_postdata();
+                } ?>
+                <?php if ($languages) {
+                    $post = get_post($languages);
+                    setup_postdata($post);
+                    get_template_part('template-parts/content', 'excerpt', ['section' => 'languages']);
+                    wp_reset_postdata();
+                } ?>
+            </div>
+        </div><!-- end row -->
+    </div>
+</section>
+    <!-- end refactor-->
 
     <!--Call-To-Action Section-->
     <section class="section bg-dark py-5">
@@ -83,26 +174,19 @@ get_header();
     </section><!--end Call-To-Action Section-->
     
      <!-- Portfolio Section -->
-     <section class="section bg-custom-gray" id="portfolio">
+
+     <section class="section bg-custom-gray" id="Portfolio">
         <div class="container">
             <h1 class="mb-5"><span class="text-danger">My</span> Portfolio</h1>
             <div class="portfolio">
                 <div class="filters">
-                    <a href="#" data-filter=".all" class="active">
-                        All
-                    </a>
-                    <a href="#" data-filter=".advertising">
-                        Advertising
-                    </a>
-                    <a href="#" data-filter=".branding">
-                        Branding
-                    </a>
-                    <a href="#" data-filter=".web">
-                        Web
-                    </a>
+                    <a href="#portfolio" data-filter=".all" class="active">All</a>
+                    <a href="#portfolio" data-filter=".woocommerce">WooCommerce</a>
+                    <a href="#portfolio" data-filter=".wordpress">WordPress</a>
+                    <a href="#portfolio" data-filter=".ecommerce">Dropshipping e-Commerce</a>
                 </div>
-                <div class="portfolio-container"> 
-<?php 
+                <div class="portfolio-container">
+<?php
                     $portfolio = new WP_query(array(
                     'post_type' => 'portfolio'
                 ));
@@ -118,17 +202,18 @@ get_header();
                     }
                 } else {
 
-                    get_template_parts('template-parts/content', 'none');
+                    get_template_part('template-parts/content', 'none');
 
                 }
                 ?>
                 </div><!-- end portfolio-container -->
-            </div> <!-- end portfolio --> 
-        </div> <!-- end container -->           
+            </div><!-- end portfolio -->
+        </div><!-- end container -->           
     </section>
-    <!-- End of portfolio section -->
+   <!-- End of portfolio section -->
+
    <!-- Contact Form -->
-   <div class="section contact" id="contact">
+   <div class="section contact" id="Contact">
         <div id="map" class="map"></div>
         <div class="container">
             <div class="row">
